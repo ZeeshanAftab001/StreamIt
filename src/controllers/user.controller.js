25d68a4acef3bbbc02a484438f8dc91514a37bf8
ZeeshanAftab001 <zeeshanaftababbasi@gmail.com>
2026-08-17 16:12:59 +0500

    User Controller Implemented.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,57 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import {APIError} from "../utils/apiError.js"
+import {APIResponse} from "../utils/apiResponse.js"
+import {User} from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler( async (req,res) =>{
-    res.status(200).json({
-        message : "ok"
-    })
-})
+    const {username,fullName,email,password}=req.body
+    
+    if ([username, fullName, email, password].some((field) => field === undefined || field === null || field.trim() === "")) {
+    throw new APIError(401, "All fields are required.");
+    }
 
+    const existedUser=await User.findOne({
+        $or : [{username},{email}]
+    })
+
+    if(existedUser){
+        throw new APIError(409,"email or username already exists!")
+    }
+    const avatarLocalPath = req.files?.avatar?.[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage?.[0]?.path;   
+
+    if(!avatarLocalPath){
+        throw new APIError(404,"Avatar Image not found")
+    }
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImage_local_path)
+
+    if(!avatar){
+        throw new APIError(404,"Avatar Image not found")
+    }
+
+    const user=await User.create({
+            username : username.toLowerCase(),
+            fullName : fullName,
+            email : email,
+            password :password,
+            avatar : avatar.url,
+            coverImage : coverImage?.url || ""
+        }
+    )
+
+    const createdUser=await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new APIError(401,"Something went wrong during creation of User!")
+    }
+
+    res.status(200).json(
+        new APIResponse(201,"User created sucessfully!",createdUser)
+    )
+})
 
 export {registerUser}
